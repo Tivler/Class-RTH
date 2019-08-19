4c60ff493421252baf103922b6c10eaa497f3d33
tivler <thomasdmckyer@gmail.com>
2019-08-19 15:41:56 -0400

    added updated assignment

--- code-activity/code-activity-3.js
+++ code-activity/code-activity-3.js
@@ -1,7 +1,13 @@
 const rvColors = ["red", "black", "grey", "white"];
 
-const newColors = rvColors.join()
+let newColors = rvColors.map(color => {
+    let [first , ...rest] = color
+    first = first.toUpperCase()
+    return [first, ...rest].join("")
+})
 
-const colors = newColors.replace(/,/g, ' ');
+const newColor = newColors.join()
+
+const colors = newColor.replace(/,/g, ' ');
 
 console.log(colors);
